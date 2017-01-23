95f58a2a8600c4fab94210dbe549f4ae086fa27a
xespana <apple@macintosh-8.risd.edu>
2017-01-23 12:42:56 -0500

    exercise12

--- exercises/exercise12/j.js
+++ exercises/exercise12/j.js
@@ -1,34 +1,8 @@
 $(document).ready(function() {
-  var hText = $("h3").text();
-
-  console.log(hText);
-
-  $("#h3Trigger").click(function(){
-      $("h3").text("This is replaced text");
-      $("h2").append(" this is added via jQuery");
-      
-      var h3TextColor = $("h3").css("color");
-      console.log("The heading 3 text color is " + h3TextColor);
-
-      $("h3").css("color", "red");
-      h3TextColor = $("h3").css("color");
-      console.log('After changing, "change" the heading 3 text color is' + h3TextColor);
-  });
-
-  $(".dropdown").click(function(){
-  	var text = $("dd",this);
-  	var icon = $(this).children('.toggle');
-    
-    text.slideToggle(400, function (){
-    	if (text.is(':visible')) {
-    	             icon.text('–');                
-    	        } else {
-    	             icon.text('+');                
-    	        }        
-      console.log("dropdown opened");
+    $("#hide").click(function(){
+        $("p").hide();
     });
-
-    $(this).toggleClass("selected");
-  });
+    $("#show").click(function(){
+        $("p").show();
+    });
 });
-console.log(8+8);
