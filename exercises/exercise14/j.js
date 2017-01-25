79677e2cc194dc98a6799aacd6b6194192e99221
xespana <apple@macintosh-8.risd.edu>
2017-01-25 18:13:10 -0500

    project3

---
+++ exercises/exercise14/j.js
@@ -0,0 +1,3 @@
+$( document ).ready(function() {
+for (var line = "#"; line.length < 8; line += "#")
+  console.log(line);
---
+++ exercises/exercise14/j2.js
@@ -0,0 +1,21 @@
+$( document ).ready(function() {
+
+var dessertNames = ["Brownies" , "Pineapple Upside Down Cake" , "Cheesecake" ,"Hot Fudge Sundae" ,"Cookie", "Donut" ,"Poundcake", "Flan", "Boysenberry Pie", "Peach Cobbler"];
+console.log(dessertNames[1]);
+console.log(dessertNames[2]);
+console.log(dessertNames[3]);
+console.log(dessertNames[4]);
+console.log(dessertNames[5]);
+console.log(dessertNames[6]);
+console.log(dessertNames[7]);
+console.log(dessertNames[8]);
+console.log(dessertNames[9]);
+console.log(dessertNames[10]);
+
+
+
+var single = "";
+for (var i = 0; i < words.length; i++) {
+  single += words[i] + " ";
+}
+console.log(single);
